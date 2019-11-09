53d49648a8ccabb345526c280ee00aecb60b574f
Yamanin3 <yamanin38@gmail.com>
2019-11-09 13:47:45 +0900

    メインメニュー画面のフォントを変更

--- SalesManagementSystem/SalesManagementSystem/MainMenu.Designer.cs
+++ SalesManagementSystem/SalesManagementSystem/MainMenu.Designer.cs
@@ -30,12 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -55,45 +55,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "TODO :)";
             // 
-            // label1
+            // button5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("pix M 8pt", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(321, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 44);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Main menu";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(174, 31);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(371, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "顧客リスト";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(174, 60);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(371, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "会員リスト";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(174, 89);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(371, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "注文リスト";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button5.Location = new System.Drawing.Point(172, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(371, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "入荷予定リスト";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -105,15 +75,45 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // button3
             // 
-            this.button5.Location = new System.Drawing.Point(172, 149);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(371, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "入荷予定リスト";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button3.Location = new System.Drawing.Point(174, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(371, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "注文リスト";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(371, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "会員リスト";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(371, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "顧客リスト";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harlow Solid Italic", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(318, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 61);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Main menu";
             // 
             // pictureBox1
             // 
