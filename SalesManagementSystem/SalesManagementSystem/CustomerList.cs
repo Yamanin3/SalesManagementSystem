26a09bd171039bb2031f972fdc124b126f6411b1
Yamanin3 <39796520+Yamanin3@users.noreply.github.com>
2019-11-13 14:00:32 +0900

    会員管理画面完成。datagridviewの行以外をクリックした場合に落ちる現象を修正。

--- SalesManagementSystem/SalesManagementSystem/CustomerList.cs
+++ SalesManagementSystem/SalesManagementSystem/CustomerList.cs
@@ -38,17 +38,22 @@ namespace SalesManagementSystem
 
         private void dataGridView1_SelectionChanged(object sender, EventArgs e)
         {
-            
-            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
-            textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
-            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
-            comboBox1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
-            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
-            textBox4.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
-            textBox5.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
-            textBox6.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
-            textBox7.Text = dataGridView1.CurrentRow.Cells[9].Value.ToString();
-
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+            else
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                comboBox1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+                textBox4.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+                textBox5.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+                textBox6.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
+                textBox7.Text = dataGridView1.CurrentRow.Cells[9].Value.ToString();
+            }
         
         }
 
